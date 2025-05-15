e792a680553c65cd2c338f8db9e801265cce1ea1
Miodec <jack@monkeytype.com>
2025-05-15 20:17:30 +0200

    add key generator

--- backend/src/middlewares/rateLimit.ts
+++ backend/src/middlewares/rateLimit.ts
@@ -1,8 +1,19 @@
+import { Request, Response } from "express";
 import rateLimit from "express-rate-limit";
 
+const getKey = (req: Request, _res: Response): string => {
+  return (
+    (req.headers["cf-connecting-ip"] as string) ||
+    (req.headers["x-forwarded-for"] as string) ||
+    (req.ip as string) ||
+    "255.255.255.255"
+  );
+};
+
 export const globalRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 10 * 60 * 1000, // 10 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: getKey,
 });
